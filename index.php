7e73ed85094079e663e358a1f7aa7aeafefe3f48
Shigekazu Aoyagi <saoyagi2@gmail.com>
2023-01-06 09:47:41 +0900

    DB作成処理実装

--- index.php
+++ index.php
@@ -1,2 +1,66 @@
 <?php
 require 'config.php';
+
+$db = new DB($config['db_path']);
+
+class DB {
+  /** @var PDO */
+  private $db_conn = NULL;
+
+  /**
+   * コンストラクタ
+   *
+   * @param string $db_path DBファイルへのパス
+   */
+  public function __construct(string $db_path)
+  {
+    try {
+      $this->connect_database($db_path);
+      $this->create_schema();
+    }
+    catch(PDOException $e) {
+      print $e->getMessage();
+      exit();
+    }
+  }
+
+  /**
+   * DB接続
+   *
+   * @param string $db_path DBファイルへのパス
+   */
+  private function connect_database(string $db_path) : void
+  {
+    $this->db_conn = new PDO('sqlite:' . __DIR__ . '/' . $db_path, NULL, NULL, [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+  }
+
+  /**
+   * スキーマ作成
+   */
+  private function create_schema() : void
+  {
+    $this->db_conn->exec("CREATE TABLE IF NOT EXISTS articles(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      date TEXT UNIQUE NOT NULL,
+      message TEXT
+    )");
+    $this->db_conn->exec("CREATE INDEX IF NOT EXISTS article_idx ON articles (date)");
+  }
+
+  /**
+   * クエリ実行
+   *
+   * @param string $sql クエリ
+   * @param array $params パラメータ
+   * @return array レコード配列
+   */
+  function query(string $sql, array $params = []) : array
+  {
+    $stmt = $this->db_conn->prepare($sql);
+    $stmt->execute($params);
+    return($stmt->fetchAll());
+  }
+}
