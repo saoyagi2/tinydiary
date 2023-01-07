e04682b6ddff1c44234c45357da79e6a92141e83
Shigekazu Aoyagi <saoyagi2@gmail.com>
2023-01-08 08:28:12 +0900

    App, View の骨組み実装

--- index.php
+++ index.php
@@ -1,7 +1,87 @@
 <?php
 require 'config.php';
 
-$db = new DB($config['db_path']);
+{
+  $app = new App($config);
+  $app->run();
+  exit();
+}
+
+class App {
+  /** @var DB */
+  private $db;
+
+  /**
+   * コンストラクタ
+   *
+   * @param array $config 設定情報
+   */
+  public function __construct(array $config)
+  {
+    $this->db = new DB($config['db_path']);
+  }
+
+  /**
+   * 実行
+   */
+  public function run() : void
+  {
+    switch($_REQUEST['mode']) {
+      case 'edit':
+        $this->edit();
+        break;
+      case 'update':
+        $this->update();
+        break;
+      case 'view':
+      default:
+        $this->view();
+    }
+  }
+
+  /**
+   * 日記表示画面
+   */
+  private function view() : void
+  {
+    $articles = $this->db->query("SELECT * FROM articles");
+    View::display_view($articles);
+  }
+
+  /**
+   * 日記編集画面
+   */
+  private function edit() : void
+  {
+  }
+
+  /**
+   * 日記更新
+   */
+  private function update() : void
+  {
+  }
+}
+
+class View {
+  /**
+   * 表示画面
+   *
+   * @param array $articles 日記データ
+   */
+  public static function display_view(array $articles) : void
+  {
+  }
+
+  /**
+   * 編集画面
+   *
+   * @param array $article 日記データ
+   */
+  public static function display_edit(array $article) : void
+  {
+  }
+}
 
 class DB {
   /** @var PDO */
